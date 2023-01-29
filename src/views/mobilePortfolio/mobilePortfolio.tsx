4749486fbf1df2fa851254343fb77e840eb41b56
Andreas <andreas95560@gmail.com>
2023-01-29 19:29:01 +0100

    responsive

--- src/views/mobilePortfolio/mobilePortfolio.tsx
+++ src/views/mobilePortfolio/mobilePortfolio.tsx
@@ -1,4 +1,4 @@
-import { Parallax } from "@react-spring/parallax";
+import { Parallax,  ParallaxLayer } from "@react-spring/parallax";
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar"
 import Portfolio from './../../components/portfolio/portfolio';
@@ -6,8 +6,13 @@ import Portfolio from './../../components/portfolio/portfolio';
 const MobilePortfolio = () => {
      return (
           <>
-               <Parallax pages={1.5} className='scroll-auto'>
-                    <Portfolio />
+               <Parallax pages={3} className='scroll-auto'>
+                    <ParallaxLayer offset={0} speed={0.1}>
+                         <img className="w-80 h-80" src="https://media.discordapp.net/attachments/978413517780697141/1035330157323485194/unknown.png" alt="" />
+                    </ParallaxLayer>
+                    <ParallaxLayer offset={0.4} speed={0.5}>
+                         <Portfolio />
+                    </ParallaxLayer>
                </Parallax>
           </>
      );
